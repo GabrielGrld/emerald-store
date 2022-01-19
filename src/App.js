9bfc4c7df4c8995c291c4649d3702f2eaaaef7af
GabrielGrld <gbrlgrld@gmail.com>
2022-01-19 15:19:55 -0500

    refactor code inside App.js to use React-router-dom, add some styles to App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from "./components/NavBar"
-import Card from "./components/Card"
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { useState } from 'react';
 
+
+//Components
+import NavBar from "./components/NavBar"
+import Home from './pages/home/Home'
+import Product from './pages/product/Product';
+import ProductCategory from './pages/productscat/ProductCategory';
+
+
+//Styles
+import './App.css';
+
+const pages = ['Collares', 'Pulceras', 'Pendientes', 'Esmeraldas'];
 
 function App() {
+const [activeSection, setActiveSection] = useState('home');
+
+
   return (
     <div className="App">
-    <NavBar />    
-      <header className="App-header">      
-      </header>
+    <div className="app-header">     
+      <BrowserRouter>
+       <NavBar />
+         <Switch>
+          <Route exact path = "/">
+            <Home />
+          </Route>
+          <Route  path = "/producto/:id">
+            <Product />
+          </Route>
+          <Route  path = "/productos/:category">
+            <ProductCategory />
+          </Route>
+          <Route  path = "*">
+          <Redirect to="/"/>
+          </Route>
+
+
+         </Switch>
+
+      </BrowserRouter>
+    
+
+
+
+        </div>
+      <footer>
+        <div style={{"minHeight":"1vh"}}><p>Emerald Store</p></div>
+      </footer>
       
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
     </div>
   );
 }
