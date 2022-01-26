3805bd2222416cbe90168ce53f79bda8ed4970e7
GabrielGrld <gbrlgrld@gmail.com>
2022-01-26 10:53:45 -0500

    add filter function to documents, only to get documents that are not sold, update carousel import

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,6 +8,10 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
+//Components
+import Carousel from '../../components/Carousel'
+
+
 //styles
 import './Home.css'
 
@@ -23,35 +27,43 @@ function Home(){
   const {documents, error} = useCollection('productos')
   console.log(documents)
 
+  // if is done to wait the documents, since useCollection Hook fetchs the data, once the data is
+  //ready, Filter is done to check what products are sold (true), or not sold (false). This will show 
+  // in the pages only the productos that have not been sold (sold===false)
+  if(documents){
+    var products = documents.filter(product => product.sold===false)
+    
+  }
+
     return(
+      
     <div className="container" style={{"margin":"1em"}}>
+    <Carousel />
+    
     <div>
     <h1>Esmeraldas Colombianas</h1>
       
       
       <Box sx={{ flexGrow: 1 }} >
       <Grid container spacing={2} justify = "center"   >
+      {error && <p>{error}</p>}
  
-{documents && documents.map((document) => (
-  <Grid  item xs={12} sm={6} md={4} lg ={3}>
-    {/* <img src={document.imgUrl} />
-    <h1>{document.name}</h1>
-    <p>{document.details}</p>
-    <span>Ct:{' '} {document.ct}</span>
-    <span><br/>Precio:{' '} {document.price}</span> */}
+{products && products.map((document) => (   
+  (<Grid  item xs={12} sm={6} md={4} lg ={3}>    
     <div key={document.id} align="center">
     <Card  
           key = {document.imgUrl}
           title={document.name}
-          description ={document.details.substring(0,100)}
+          description ={document.details.substring(0,100)}          
           imgLink ={document.imgUrl}
           item = {document} 
           />
     </div>
-  </Grid>
+  </Grid>)
 ))}      
 
       </Grid>
+      
 
     </Box>
     </div>
